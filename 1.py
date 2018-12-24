1b2a5f8d4ee69221805b9248a30a7f80fc29a22c
Yanay Lehavi <yanay@pitch.studio>
2018-12-24 12:53:15 -0800

    ..

--- 1.py
+++ 1.py
@@ -1,4 +1,4 @@
-def ep1 (max):
+def ep1(max):
     i = 3
     total = 0
     while i < max:
@@ -7,7 +7,7 @@ def ep1 (max):
         i += 1
     return total
 
-def ep2 (max):
+def ep2(max):
     prev = 1
     f = 2
     total = 0
@@ -16,10 +16,10 @@ def ep2 (max):
         pf = f
         f = f + prev
         prev = pf
+    if f < max: total += f
     return total
 
-
-def ep3 (max):
+def ep3o(max):
     # build a list of primes
     primes = [2]
     i = 3   # to skip even numbers
@@ -37,6 +37,29 @@ def ep3 (max):
         if max % i == 0:
             return i
 
-print "ep1:", ep1(10)
+def ep3(max, primes, pfs):
+    for p in primes:
+        if max % p == 0:
+            pfs.append(p)
+            return ep3(max/p, primes, pfs)
+
+    np = primes[-1] + 1
+    while np <= max:
+        notp = 0
+        for x in primes:
+            if np % x == 0:
+                notp = 1
+                break
+        if notp:
+            np += 1
+        else:
+            primes.append(np)
+            if max % np == 0:
+                pfs.append(np)
+            return ep3(max, primes, pfs)
+
+    return pfs[-1]
+
+print "ep1:", ep1(1000)
 print "ep2:", ep2(4*1000*1000)
-print "ep3:", ep3(600809)
+#print "ep3:", ep3(901, primes=[2], pfs=[])
