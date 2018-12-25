19c49b2e4dfb4875ca79907b58cb18ea6029b039
Yanay <lehavi@yahoo.com>
2018-12-25 09:37:47 -0800

    solves #3

--- 1.py
+++ 1.py
@@ -19,47 +19,45 @@ def ep2(max):
     if f < max: total += f
     return total
 
-def ep3o(max):
-    # build a list of primes
-    primes = [2]
-    i = 3   # to skip even numbers
-    while i < max/2:  # past /2 won't be a factor
-        notp = 0
-        for j in primes:
-            if i % j == 0:
-                notp = 1
-                break
-        if notp == 0: primes.append(i)
-        i += 2
+def ReduceN(n, prime):
+    success = 1
+    while success:
+        success = 0
+        if n % prime == 0:
+            success = 1
+            n /= prime
+    return n
 
-    # find the largest prime factor of max
-    for i in reversed(primes):
-        if max % i == 0:
+def NextPrime(primes):
+    i = primes[-1] + 1
+    tryagain = 0
+    while 1:
+        for p in primes:
+            if i % p == 0:
+                tryagain = 1
+                break
+        if tryagain:
+            i += 1
+            tryagain = 0
+        else:
             return i
 
-def ep3(max, primes, pfs):
-    for p in primes:
-        if max % p == 0:
-            pfs.append(p)
-            return ep3(max/p, primes, pfs)
+def ep3(n):
+    primes = [2]
+    topgun = 1
+    newp = 1
 
-    np = primes[-1] + 1
-    while np <= max:
-        notp = 0
-        for x in primes:
-            if np % x == 0:
-                notp = 1
-                break
-        if notp:
-            np += 1
-        else:
-            primes.append(np)
-            if max % np == 0:
-                pfs.append(np)
-            return ep3(max, primes, pfs)
+    while newp <= n:
+        for p in primes:
+            r = ReduceN(n, p)
+            if r < n: topgun = p
+            n = r
 
-    return pfs[-1]
+        newp = NextPrime(primes)
+        primes.append(newp)
+
+    return topgun
 
 print "ep1:", ep1(1000)
 print "ep2:", ep2(4*1000*1000)
-#print "ep3:", ep3(901, primes=[2], pfs=[])
+print "ep3:", ep3(600851475143)
